385465ae226887b2e354eda6154207657db06d4c
Kristjan <nigathr@gmail.com>
2025-03-07 12:35:06 +0100

    Add very secure encryption

--- Case/Models/Student.cs
+++ Case/Models/Student.cs
@@ -1,11 +1,15 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Serializers;
 
 namespace Case.Models;
 public class Student {
 	[BsonId]
 	[BsonRepresentation(BsonType.ObjectId)]
 	public string? Id { get; set; }
+
+	[BsonSerializer(typeof(CprSerializer))]
 	public string Cpr { get; set; }
 	public string Name { get; set; }
 	public string Email { get; set; }
@@ -16,4 +20,19 @@ public class Student {
 public class FinishedCourse {
 	public string Title { get; set; }
 	public string Grade { get; set; }
+}
+
+public class CprSerializer : SerializerBase<string> {
+	public override string Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) {
+		var value = context.Reader.ReadString();
+		if (value.Length > 21) // hack to support cpr values from before the "encryption"
+			return value.Substring(21);
+		return value;
+		// TODO: implement proper encryption
+	}
+
+	public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, string value) {
+		context.Writer.WriteString("definitely not a cpr " + value);
+		// TODO: implement proper encryption
+	}
 }
